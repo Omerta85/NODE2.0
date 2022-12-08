c0ae3aba1e89972c8aca2b1f0a511cec67e0feb4
Omerta85 <666Omerta@gmail.com>
2022-12-08 18:52:22 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+const userRouter = require('./router/user.router');
+const authRouter = require('./router/auth.router');
+const configs = require('./config/config');
+
+const app = express();
+
+mongoose.set('strictQuery', false);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/auth', authRouter);
+app.use('/users', userRouter);
+
+app.get('/', (req, res) => {
+    res.json('WELOCME')
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message || 'Unknown error',
+        status: err.status || 500
+    });
+});
+
+app.listen(configs.PORT, async () => {
+    await mongoose.connect(configs.MONGO_URL);
+    console.log(`Server listen ${configs.PORT}`);
+});
